5584365f812ede989ed1b5d334c71d58a10a2797
ra8200 <ra8200@hotmail.com>
2021-08-18 14:55:09 -0400

    can add stats

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,7 +15,11 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  db.Workout.Stats()
+  db.Workout.aggregate([
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .sort({ day: -1 })
+    .limit(7)
     .then((dbData) => {
       res.json(dbData);
     })
